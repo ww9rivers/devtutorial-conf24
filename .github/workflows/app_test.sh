3c2187874bcacd2e4bd407382bd5bd3280121b02
bdebek-splunk <128054592+bdebek-splunk@users.noreply.github.com>
2024-06-06 15:46:26 +0200

    Dev (#2) Updated tests and comments in the script
    * add latest test
    
    * add dev branch to workflow events
    
    * add splunklib to requirements.txt
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * add pytest
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * add test-latest to required tests for release
    
    * add comments in test script, add pytest scenarios
    
    * fix test.sample
    
    * add pytest report generation
    
    * add pytest-html
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * delete version variable from build job
    
    * small fix
    
    * small fix
    
    * add app name extraction
    
    * add app name extraction
    
    * update README.md

--- .github/workflows/app_test.sh
+++ .github/workflows/app_test.sh
@@ -2,7 +2,8 @@
 
 #set -x
 
-APP_ROOT="devtutorial_conf24"
+# ENVIROMENT VARIABLES
+APP_ROOT=$(jq -r '.meta.name' ./globalConfig.json)
 APPS_DIR="/opt/splunk/etc/apps"
 USER="admin"
 PASSWORD="password"
@@ -10,11 +11,11 @@ CI_PROJECT_DIR=${CI_PROJECT_DIR:-`pwd`}
 CONTAINER_NAME="splunk"
 SPLUNK_VERSION=$1
 
+# SETTING UP DOCKER CONTAINER WITH SPLUNK
 echo -e "\033[92m Installing docker...\033[0m"
 apt-get update
 apt-get install docker.io -y
 
-# echo -e "\033[92m Pulling splunk docker image...\033[0m"
 
 echo -e "\033[92m Creating splunk container...\033[0m"
 docker run --rm -d -p 8000:8000 -p 8089:8089 -e "SPLUNK_START_ARGS=--accept-license" -e "SPLUNK_PASSWORD=$PASSWORD" --name $CONTAINER_NAME splunk/splunk:$SPLUNK_VERSION
@@ -27,7 +28,7 @@ echo "My splunk instance host: $CONTAINER_IP:8089"
 
 echo -e "\033[92m Waiting for splunk to be up...\033[0m"
 
-
+# COPYING APP DATA FROM ARTIFACT TO APPS FOLDER IN THE CONTAINER
 echo -e "\033[92m Installing app...\033[0m"
 ls -l $CI_PROJECT_DIR
 FILE_NAME=$(ls -1 app-dir/)
@@ -42,17 +43,16 @@ docker exec -i -u root $CONTAINER_NAME chmod -R 777 $APPS_DIR/
 docker exec -i $CONTAINER_NAME ls -l $APPS_DIR/
 docker exec -i $CONTAINER_NAME ls -l $APPS_DIR/$APP_ROOT/
 
-# docker exec -i $CONTAINER_NAME ls -l $APPS_DIR/$APP_ROOT/bin
-# docker exec -i $CONTAINER_NAME cat $APPS_DIR/$APP_ROOT/bin/customadd.py
-# docker exec -i $CONTAINER_NAME ls -l $APPS_DIR/$APP_ROOT/default
-# docker exec -i $CONTAINER_NAME cat $APPS_DIR/$APP_ROOT/default/commands.conf
-
+# INSTALLING pytest-splunk-addon FOR FUTURE KNOWLEDGE OBJECT TESTING
 echo "Installing python packages"
 pip install pytest-splunk-addon
 
+pip install pytest-html
+
+
 echo "My splunk instance host: $CONTAINER_IP:8000"
 
-# Check running docker containers
+# CHECKING IF SPLUNK CONTAINER IS RUNNING
 echo -e "\033[92m Checking running containers... \033[0m"
 docker ps
 
@@ -78,18 +78,20 @@ while [[ $loopCounter != 0 && $mainReady != 1 ]]; do
     echo -e "\033[92m APP LIST: $appList\033[0m"
 
     if [[ $checked != 1 ]]; then
+
+        # WHEN CONTAINER IS UP WE CREATE HEC TOKEN FOR TESTING PURPOSES - SENDING DUMMY DATA TO SPLUNK
+
+
         echo "Creating HEC token..."
         HEC_TOKEN_OUTPUT=$(docker exec -i -u splunk $CONTAINER_NAME bash -c "SPLUNK_USERNAME=$USER SPLUNK_PASSWORD=$PASSWORD /opt/splunk/bin/splunk http-event-collector create new-token -uri https://$CONTAINER_IP:8089  -disabled 0 -index log")
         HEC_TOKEN=$(echo "$HEC_TOKEN_OUTPUT" | grep -oP 'token=\K[^ ]+')
         echo "Generated token: $HEC_TOKEN"
 
-        echo -e "\033[92m Checking Splunk endpoints...\033[0m"
 
-        echo -e "\033[92m Looking for $APP_ROOT app...\033[0m"
-        curl -k -u $USER:$PASSWORD -i -q https://$CONTAINER_IP:8089/services/apps/local/?search=$APP_ROOT | grep $APP_ROOT
 
         echo -e "\033[92m Running unit tests...\033[0m"
 
+        # CHECKING IF APP IS CORRECTLY INSTALLED
         echo -e "\033[92m Checking if app is installed... \033[0m"
         if ! curl -k -u $USER:$PASSWORD -i -q https://$CONTAINER_IP:8089/services/apps/local/?search=$APP_ROOT | grep -q $APP_ROOT; then
           echo "App $APP_ROOT not found in local apps!"
@@ -100,6 +102,7 @@ while [[ $loopCounter != 0 && $mainReady != 1 ]]; do
 
         echo "______________________________________________________________________"
 
+        # CHECKING IF "CUSTOMADD" COMMAND RETURNS CORECT RESULT
         echo -e "\033[92m Checking if custom search command runs correctly... \033[0m"
         echo -e "\033[92m Adding 2 to 999 and expecting 1001... \033[0m"
 
@@ -117,9 +120,13 @@ while [[ $loopCounter != 0 && $mainReady != 1 ]]; do
 
         echo "______________________________________________________________________"
 
+
+        # RUNNING KNOWLEDGE OBJECTS TESTS USING PYTEST AND GENERATED DUMMY DATA
         echo -e "\033[92m Running Knowledge Object Tests... \033[0m"
 
-        pytest -v $CI_PROJECT_DIR/tests/knowledge/test_savedsearches.py --splunk-type=external --splunk-app=$CI_PROJECT_DIR/package/ --splunk-data-generator=$CI_PROJECT_DIR/tests/knowledge/ --splunk-host=$CONTAINER_IP --splunk-port=8089 --splunk-user=$USER --splunk-password=$PASSWORD --splunk-hec-token=$HEC_TOKEN
+        set -e # fail the job if pytest resutls with failures
+
+        pytest $CI_PROJECT_DIR/tests/knowledge/test_savedsearches.py --splunk-type=external --splunk-app=$CI_PROJECT_DIR/package/ --splunk-data-generator=$CI_PROJECT_DIR/tests/knowledge/ --splunk-host=$CONTAINER_IP --splunk-port=8089 --splunk-user=$USER --splunk-password=$PASSWORD --splunk-hec-token=$HEC_TOKEN --html=pytest-report.html --self-contained-html
 
         echo "______________________________________________________________________"
 
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Demo app for "Git your apps together" session at .conf24
 
 Test
 Test 2
+Test 3
